74d610c07304fc4b9da46d11615956e423ccd865
Sergei Korchemkin <hellk.pub@gmail.com>
2018-06-03 01:23:02 +0300

    Switch global variables to UPPERCASE

--- GRAPHLAB.C
+++ GRAPHLAB.C
@@ -18,7 +18,7 @@
 #define DELAY 10
 
 /* Global variables declaration */
-int x0, y0, stepx, stepy;
+int X0, Y0, STEPX, STEPY;
 
 /* Initializes graphics driver and global variables */
 void init(void);
@@ -78,23 +78,23 @@ void init(void)
     }
     
     /* Initializing global variables */
-    x0 = getmaxx() / 2;
-    y0 = getmaxy() / 2;
-    stepx = getmaxx() / SCALEX;
-    stepy = getmaxy() / SCALEY;
+    X0 = getmaxx() / 2;
+    Y0 = getmaxy() / 2;
+    STEPX = getmaxx() / SCALEX;
+    STEPY = getmaxy() / SCALEY;
 }
 
 void draw_parabola(float xmin, float xmax, float a, float b, float c)
 {
     float x, y, one;
-    one = stepy; /* placeholder variable */
+    one = STEPY; /* placeholder variable */
     
     /* determine the leftmost Y to start drawing from */
     y = a * xmin * xmin + b * xmin + c;
     /* move current position to the starting point
       (x * one) converts x value to global coordinates
-      (x0 + x * one) offsets it by where 0,0 point is */
-    moveto(x0 + xmin * one, y0 - y * one);
+      (X0 + x * one) offsets it by where 0,0 point is */
+    moveto(X0 + xmin * one, Y0 - y * one);
     
     /* for all values from xmin to xmax, differentiating by STEP */
     for (x = xmin + STEP; x <= xmax; x += STEP)
@@ -102,7 +102,7 @@ void draw_parabola(float xmin, float xmax, float a, float b, float c)
         /* calculate Y for the point */
         y = a * x * x + b * x + c;
         /* draw a line from current position to new X,Y point */
-        lineto(x0 + x * one, y0 - y * one);
+        lineto(X0 + x * one, Y0 - y * one);
         delay(DELAY);
     }
 }
@@ -110,20 +110,20 @@ void draw_parabola(float xmin, float xmax, float a, float b, float c)
 void draw_cycloid(float tmin, float tmax, float a, float b)
 {
     float x, y, t, one;
-    one = stepy; /* Placeholder variable */
+    one = STEPY; /* Placeholder variable */
     
     /* determine the starting drawing point */
     x = a * tmin - b * sin(tmin);
     y = a - b * cos(tmin);
     /* move current position to the starting point */
-    moveto(x0 + x * one, y0 - y * one);
+    moveto(X0 + x * one, Y0 - y * one);
     /* for all values from tmin to tmax, differentiating by STEP */
     for (t = tmin + STEP; t <= tmax; t += STEP)
     {
         x = a * t - b * sin(t);
         y = a - b * cos(t);
         /* draw a line from current position to new X,Y point */
-        lineto(x0 + x * one, y0 - y * one);
+        lineto(X0 + x * one, Y0 - y * one);
         delay(DELAY);
     }
 }
@@ -133,46 +133,46 @@ void draw_xy_axis(int xlen, int ylen, int freq)
     int xneg, yneg, xpos, ypos, i;
     char msg[40];
 
-    xneg = x0 - stepx * xlen; /* the leftmost point of x axis */
-    xpos = x0 + stepx * xlen; /* the rightmost point of x axis */
-    yneg = y0 + stepy * ylen; /* the lowest point of y axis */
-    ypos = y0 - stepy * ylen; /* the highest point of y axis */
+    xneg = X0 - STEPX * xlen; /* the leftmost point of x axis */
+    xpos = X0 + STEPX * xlen; /* the rightmost point of x axis */
+    yneg = Y0 + STEPY * ylen; /* the lowest point of y axis */
+    ypos = Y0 - STEPY * ylen; /* the highest point of y axis */
     
-    line(xneg, y0, xpos, y0); /* draw x */
-    line(x0, yneg, x0, ypos); /* draw y */
+    line(xneg, Y0, xpos, Y0); /* draw x */
+    line(X0, yneg, X0, ypos); /* draw y */
 
     /* draw arrows and axis labels */
-    line(xpos, y0 + 2, xpos + 5, y0); /* x */
-    line(xpos + 5, y0, xpos, y0 - 2);
-    outtextxy(xpos - 5, y0 - textheight("x") - 10, "x");
+    line(xpos, Y0 + 2, xpos + 5, Y0); /* x */
+    line(xpos + 5, Y0, xpos, Y0 - 2);
+    outtextxy(xpos - 5, Y0 - textheight("x") - 10, "x");
 
-    line(x0 - 2, ypos, x0, ypos - 5); /* y */
-    line(x0, ypos - 5, x0 + 2, ypos);
-    outtextxy(x0 - textwidth("y") - 10, ypos - 5, "y");
+    line(X0 - 2, ypos, X0, ypos - 5); /* y */
+    line(X0, ypos - 5, X0 + 2, ypos);
+    outtextxy(X0 - textwidth("y") - 10, ypos - 5, "y");
 
     /* draw values along the x axis */
-    for (i = xneg; i <= xpos; i += stepx)
+    for (i = xneg; i <= xpos; i += STEPX)
     {
         /* draw every 5th value */
-        if (((i - x0) / stepx) % freq != 0)
+        if (((i - X0) / STEPX) % freq != 0)
             continue;
-        line(i, y0 - 2, i, y0 + 2);
-        sprintf(msg, "%d", (i - x0) / stepx);
-        outtextxy(i - textwidth(msg) / 2, y0 + 5, msg);
+        line(i, Y0 - 2, i, Y0 + 2);
+        sprintf(msg, "%d", (i - X0) / STEPX);
+        outtextxy(i - textwidth(msg) / 2, Y0 + 5, msg);
     }
     /* draw values along the y axis
        in math Y grows from bottom to top,but in the graphics
        library global Y values increase from top to bottom */
-    for (i = yneg; i >= ypos; i -= stepy)
+    for (i = yneg; i >= ypos; i -= STEPY)
     {
-        if (((y0 - i) / stepy) % freq != 0)
+        if (((Y0 - i) / STEPY) % freq != 0)
             continue;
         /* already placed label for 0 while drawing X values */
-        if (i == y0)
+        if (i == Y0)
             continue;
-        line(x0 - 2, i, x0 + 2, i);
-        sprintf(msg, "%d", (y0 - i) / stepy);
-        outtextxy(x0 + 5, i - textheight(msg) / 2, msg);
+        line(X0 - 2, i, X0 + 2, i);
+        sprintf(msg, "%d", (Y0 - i) / STEPY);
+        outtextxy(X0 + 5, i - textheight(msg) / 2, msg);
     }
 }
 
