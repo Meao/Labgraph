7897ab91e55205724942ec07ee4d8b6c469bce06
Sergei Korchemkin <hellk.pub@gmail.com>
2018-06-02 22:42:38 +0300

    Add 4.C: rectangle inscribed in a circle, circle is changing colors

---
+++ 4.C
@@ -0,0 +1,39 @@
+/* Graphics in MSDOS Borland Turbo C 2.01
+   https://www.cs.colorado.edu/~main/bgi/doc/
+   Coordinates start from LEFT UPPER corner
+   and are counted in PIXELS.
+   These will be referred to as GLOBAL coordinates.
+*/
+
+#include <stdio.h>
+#include <graphics.h>
+#include <math.h>
+
+int main()
+{
+    int midx, midy, color, rad, gd = DETECT, gm;
+    float halfside;
+    
+    initgraph(&gd, &gm, "");
+    
+    midx = getmaxx() / 2;
+    midy = getmaxy() / 2;
+    
+    setbkcolor(WHITE);
+    rad = 100;
+    halfside = floor(rad * cos(45 * M_PI / 180)); /* b = 2a * cos(45) */
+    
+    setcolor(BLUE);
+    rectangle(midx - halfside, midy - halfside, midx + halfside, midy + halfside);
+    
+    for (color = 1; color < 15; color++)
+    {
+        setcolor(color);
+        circle(midx, midy, rad);
+        delay(500);
+    }
+    
+    getch();
+    closegraph();
+    return 0;
+}
